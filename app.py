8b6aa15752c4d36bf2c9b889e4c5e01e9287b78d
shinewan <tel18810162535@163.com>
2023-03-20 11:36:33 +0800

    添加中文描述

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def show_messages(text):
 
 BASE_PROMPT = [
     {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "system", "content": "对于提问的语言，你将偏向相同的语言的文化思维进行回答."},
+    {"role": "system", "content": "对于提问的语言，你将偏向相同的语言文化思维进行回答."},
     {"role": "system", "content": "适当多给出一些建议."}
 ]
 
@@ -19,10 +19,10 @@ if "messages" not in st.session_state:
 st.subheader("ChatGPT @OneSun Personal Public Platform")
 
 openai.api_key = st.text_input("Paste your OpenAI Key here",value="", type ="password")
-prompt = st.text_input("Prompt",value = "Enter your message here...")
+prompt = st.text_input("Prompt",value = "输入你的问题...")
 
 
-if st.button("Send"):
+if st.button("发送"):
     with st.spinner("Generating response..."):
         st.session_state["messages"] += [{"role": "user", "content": prompt}]
         response = openai.ChatCompletion.create(
@@ -38,7 +38,7 @@ if st.button("Send"):
         st.session_state["messages"] += [
             {"role": "system","content": message_response}
         ]
-if st.button("Clear"):
+if st.button("清除"):
     st.session_state["messages"] = BASE_PROMPT
 
 text = st.empty()
